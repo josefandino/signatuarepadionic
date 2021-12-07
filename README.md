5d7852273d260060be3568b3f2205bedeed5b95c
josefandinod <josefandinod@gmail.com>
2021-12-06 19:32:21 -0500

    deploy

---
+++ README.md
@@ -0,0 +1,14 @@
+# Signature Pad Ionic - Cordova
+##### para utilizar:
+git clone https://github.com/josefandino/signatuarepadionic.git
+
+### instalar dependencias:
+cd signatuarepadionic
+
+npm i
+
+### instlar Signature Pad
+npm i signature_pad
+
+### inicializar server:
+ionic serve -c
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+import SignaturePad from 'signature_pad';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,48 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  signaturePad: SignaturePad;
+  firmaImg = true;
+  firma = '../../../assets/img/signature.png';
+
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  @ViewChild('canvas') canvasEl: ElementRef;
+
+  signatureImg: string;
+
+
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/use-lifecycle-interface
+  ngAfterViewInit() {
+    this.signaturePad = new SignaturePad(this.canvasEl.nativeElement);
+  }
+
+  startDrawing(event: Event) {
+    // console.log('El event', event);
+    // funciona en el dispositivo, no en el navegador
+
+  }
+
+  moved(event: Event) {
+    // funciona en el dispositivo, no en el navegador
+    // console.log('Evetnt Moved: ', event);
+  }
+
+  clearPad() {
+    this.signaturePad.clear();
+    this.firmaImg = false;
+  }
+
+  savePad() {
+    console.log('Me estas tocando');
+    const base64Data = this.signaturePad.toDataURL();
+    this.signatureImg = base64Data;
+    console.log('input file', this.inputFile);
+  }
+
+  inputFile( e ) {
+    console.log('input file', e);
+  }
 
 }
